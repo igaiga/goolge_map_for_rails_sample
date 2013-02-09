c1f49d42eb89e1d490f0e9fd09a41c842c7386db
Kuniaki IGARASHI <igaiga@gmail.com>
2013-02-09 11:13:32 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GooglemapSample::Application.config.secret_token = '7537b279f936370e8d9035196d07f3dbc1a1121a6efaa04d79dcaec254c2ed78ea1bd3bae49d20d015f3f1e927b162b31024b17d1a568807a4330564c9776c2c'
